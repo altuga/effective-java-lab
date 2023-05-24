935eb096af69bd9d1fce3fdd9eb5007628accd08
Altug B. Altintas <altuga@gmail.com>
2023-05-24 14:26:30 +0300

    q21

--- src/main/java/org/jugistanbul/question21/Calculator.java
+++ src/main/java/org/jugistanbul/question21/Calculator.java
@@ -1,6 +1,5 @@
 package org.jugistanbul.question21;
 
-
 public class Calculator {
 
     public static final double MERCURY_MASS = 3.302e+23;
@@ -12,20 +11,17 @@ public class Calculator {
     public static final double URANUS_MASS = 8.683e+25;
     public static final double NEPTUNE_MASS = 1.024e+26;
 
-
     public static final double MERCURY_REDIUS = 2.439e6;
-    public static final double VENUS_REDIUS  = 6.052e6;
-    public static final double EARTH_REDIUS  = 6.378e6;
-    public static final double MARS_REDIUS  = 3.393e6;
-    public static final double JUPITER_REDIUS  = 7.149e7;
-    public static final double SATURN_REDIUS  = 6.027e7;
-    public static final double URANUS_REDIUS  = 2.556e7;
-    public static final double NEPTUNE_REDIUS  = 2.477e7;
-
+    public static final double VENUS_REDIUS = 6.052e6;
+    public static final double EARTH_REDIUS = 6.378e6;
+    public static final double MARS_REDIUS = 3.393e6;
+    public static final double JUPITER_REDIUS = 7.149e7;
+    public static final double SATURN_REDIUS = 6.027e7;
+    public static final double URANUS_REDIUS = 2.556e7;
+    public static final double NEPTUNE_REDIUS = 2.477e7;
 
     // Universal gravitational constant in m^3 / kg s^2
     private static final double G = 6.67300E-11;
-
 
     private static double convertToMass(double wightOfPerson) {
 
@@ -35,8 +31,8 @@ public class Calculator {
 
     /**
      *
-     * @param radius   In meters
-     * @param mass  In kilograms
+     * @param radius In meters
+     * @param mass   In kilograms
      * @return
      */
     public static double calculateSurfaceWeight(double radius, double mass, double wightOfPerson) {
@@ -45,11 +41,7 @@ public class Calculator {
 
         double surfaceGravity; // In m / s^2
         surfaceGravity = G * mass / (radius * radius);
-        return wightOfPerson * surfaceGravity;  // F = ma
+        return wightOfPerson * surfaceGravity; // F = ma
     }
-
-
-
-
 
 }
--- src/main/java/org/jugistanbul/question21/Main.java
+++ src/main/java/org/jugistanbul/question21/Main.java
@@ -1,31 +1,55 @@
 package org.jugistanbul.question21;
 
-
 /**
  * TODO
- * 1 - How to calculate your  weight on all eight planets  - 5 minutes
+ * 1 - How to calculate your weight on all eight planets - 5 minutes
  * 2 - Instructor will show the solutions
- * 3 - What is the lesson  ?
+ * 3 - What is the lesson ?
  */
+
+class Value {
+    int value;
+
+    public Value(int value) {
+        this.value = value;
+    }
+}
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void mainx(String[] args) {
 
         /**
          * If your Weight is 80 KG on Earth then what will be on Mars? Or Uranus ? ...
          */
-        double earthWeight = Double.parseDouble(args[0]);
+        double earthWeight = Double.parseDouble("80");
 
-        double result = Calculator.
-                calculateSurfaceWeight(Calculator.JUPITER_REDIUS,
-                        Calculator.JUPITER_MASS, earthWeight);
+        double result = Calculator.calculateSurfaceWeight(Calculator.JUPITER_REDIUS,
+                Calculator.JUPITER_REDIUS, earthWeight);
         System.out.println("Weight on EARTH = " + result);
+        Value value = new Value(39);
+        calculate(value);
+        value.value = 48;
 
         /**
-         * for (Planets p  ...) {
-         *   .......
-         *    System.out.printf ...
+         * for (Planets p ...) {
+         * .......
+         * System.out.printf ...
          * }
          */
     }
+
+    public static void main(String[] args) {
+        double earthWeight = Double.parseDouble("80");
+        double mass = earthWeight / Planet.EARTH.surfaceGravity();
+        for (Planet p : Planet.values())
+            System.out.printf("Weight on %s is %f%n",
+                    p, p.surfaceWeight(mass));
+    }
+
+    public static double calculate(Value value) {
+        System.out.println(value);
+        return 34.2;
+    }
+
 }
